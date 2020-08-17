0369cec33927eeb6b15928b96624608ea20fe445
Emily Hartzell <emily.g.hartzell@gmail.com>
2020-08-17 14:12:45 -0500

    started to refactor to include departments

--- scripts/EmployeeHTML.js
+++ scripts/EmployeeHTML.js
@@ -2,10 +2,15 @@ export const employeeAsHTML = (employeeObj) => {
     return `
     
     <section>
-        <div>Name: ${employeeObj.firstName} ${employeeObj.lastName}</div>
-        <div>Age: ${employeeObj.age}</div>
-        <div>Computer: ${employeeObj.computer.model}</div>
-        <div>Year: ${employeeObj.computer.year}</div>
+        <header class="employee__name">
+            <h1>Name: ${employeeObj.firstName} ${employeeObj.lastName}</h1>
+        </header>
+        <section class="employee__computer">
+            Currently using a ${employeeObj.computer.year} ${employeeObj.computer.model}</div>
+        </section>
+        <section class="employee__department">
+            Works in the ${employeeObj.department.name} department
+        </section>
     </section>
     
     
--- scripts/EmployeeProvider.js
+++ scripts/EmployeeProvider.js
@@ -3,7 +3,7 @@ let employees = []
 
 
 export const getEmployees = () => {
-    return fetch('http://localhost:3000/employees?_expand=computer')
+    return fetch('http://localhost:3000/employees?_expand=department')
         .then(resp => resp.json())
         .then(parsedRes => {
             employees = parsedRes
