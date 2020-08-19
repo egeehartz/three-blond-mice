63bb1826644b0b66b19fac6a1e2c6e36e7874d4a
Emily Hartzell <emily.g.hartzell@gmail.com>
2020-08-19 15:32:44 -0500

    started the many to many process

--- scripts/EmployeeList.js
+++ scripts/EmployeeList.js
@@ -20,17 +20,29 @@ const render = (employeeArray) => {
             const computers = useComputers()
             const departments = useDepartments()
             const locations = useLocations()
+            const customers = useCustomers()
+            const customerRelationships = useEmployeeCustomers()
 
            const rep = employeeArray.map(employee => {
 
+                //.find() for 1 -> * because we need to find the ONE that matches
                 const computerFound = computers.find((computer) => computer.id === employee.computerId)
                 const deptFound = departments.find((department) => department.id === employee.departmentId)
                 const locationFound = locations.find((location) => location.id === employee.locationId)
+
+                //.filter() for * -> * because we need to find ALL that matches
+                    // Find all the customer relationships, gonna return an array of 2 customers because each employee has 2
+                    const customerEmployeeRelation = customerRelationships.filter(ec => ec.employee.id === employeeId)
+
+                    // Find the related customer for each relationship
+                    const assignedCustomers = customerEmployeeRelation.map(rel => {
+                        return customers.find(customer => customer.id === rel.customerId)
+
             
-                return employeeAsHTML(employee, computerFound, deptFound, locationFound)
-            
-            }).join("")
-            eventHub.innerHTML = rep
+                        
+                    }).join("")
+                    employeeAsHTML(employee, computerFound, deptFound, locationFound, assignedCustomers)
+                    return eventHub.innerHTML = rep
 
 
         })
